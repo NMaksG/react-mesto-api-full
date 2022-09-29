da27a9ba0bc5657c57575e445e09e32c71fbfdb0
Maksim Nayda <NMaximus@yandex.ru>
2022-09-29 06:41:57 +0300

    PR15+test

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 const { errors, celebrate, Joi } = require('celebrate');
@@ -27,6 +27,12 @@ app.use(
 );
 
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
